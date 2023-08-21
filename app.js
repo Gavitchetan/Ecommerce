9329ffe54cdf1ca5be20420cd99bcb828c46b345
dev <dev@kali.ccc>
2023-08-21 18:41:49 +0530

    PRoductUrl

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ import { config } from "dotenv";
 import { Database } from "./data/Db.js";
 import ProductRouter from "./routes/productroutes.js";
 import cors from "cors"
-
-
-
 config({
     path: "./data/info.env"
 });
@@ -15,10 +12,6 @@ app.use(cors())
 import { ErrorsMiddleware } from "./middlewares/erorr.js";
 import userRouter from "./routes/Userroutes.js";
 import cookieParser from "cookie-parser";
-
-
-
-// Initialize Express app
 
 // Connect to the database
 Database();
